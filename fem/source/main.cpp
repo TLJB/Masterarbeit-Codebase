6737d1a03b70ae0d4c5352b29cccf4206811d18f
Till Budde <TillBudde@protonmail.com>
2021-07-12 14:17:57 +0200

    Added Doxygen to main

--- fem/source/main.cpp
+++ fem/source/main.cpp
@@ -1,14 +1,29 @@
+/**
+ * @file main.cpp
+ * @author Till Budde (tilljanis.budde@tu-dortmund.de)
+ * @brief Main File of the FEM Code
+ * @version 0.1
+ * @date 2021-06-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<iostream>
 #include"toplevel.h"
 #include<boost/exception/diagnostic_information.hpp>
 
+/**
+ * @brief Execute the FEM Code
+ * 
+ * @return int 0 if succesfull, else 1
+ */
 int main()
 {
 	using namespace fem;
 	// This error catch doesn't make all that much sense
 	try 
 	{
-		TopLevel<3,3> test;
+		TopLevel<2,2> test;
 		test.run();
 		std::cout << "test run" << std::endl;
 	}
